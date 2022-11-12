f1922f03645660e5ee7e8cdc83a5c8aee42f61d6
Yassine Oudjana <y.oudjana@protonmail.com>
2022-11-12 19:57:50 +0300

    net: ipa: Define 2.* versions
    Add enum ipa_version entries for IPA v2.0, v2.5 and v2.6l. Since
    Those versions use BAM as their DMA interface.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

--- drivers/net/ipa/ipa_version.h
+++ drivers/net/ipa/ipa_version.h
@@ -8,6 +8,9 @@
 
 /**
  * enum ipa_version
+ * @IPA_VERSION_2_0:	IPA version 2.0/BAM
+ * @IPA_VERSION_2_5:	IPA version 2.5/BAM
+ * @IPA_VERSION_2_6L:	IPA version 2.6 lite/BAM
  * @IPA_VERSION_3_0:	IPA version 3.0/GSI version 1.0
  * @IPA_VERSION_3_1:	IPA version 3.1/GSI version 1.1
  * @IPA_VERSION_3_5:	IPA version 3.5/GSI version 1.2
@@ -21,10 +24,13 @@
  * @IPA_VERSION_4_11:	IPA version 4.11/GSI version 2.11 (2.1.1)
  * @IPA_VERSION_COUNT:	Number of defined IPA versions
  *
- * Defines the version of IPA (and GSI) hardware present on the platform.
+ * Defines the version of IPA (and DMA) hardware present on the platform.
  * Please update ipa_version_string() whenever a new version is added.
  */
 enum ipa_version {
+	IPA_VERSION_2_0,
+	IPA_VERSION_2_5,
+	IPA_VERSION_2_6L,
 	IPA_VERSION_3_0,
 	IPA_VERSION_3_1,
 	IPA_VERSION_3_5,
